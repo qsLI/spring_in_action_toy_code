36f5e13bd7b7bef8e32cbda0c38e05f1f038f566
liqisheng@whu.edu.cn <liqisheng@whu.edu.cn>
2016-01-14 11:31:05 +0800

    test xml based configuration and add log4j config

---
+++ spring_core/src/test/java/soundsystem/ConstructorArgCNamespaceTest.java
@@ -0,0 +1,34 @@
+package soundsystem;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by KL on 2016/1/14.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class ConstructorArgCNamespaceTest {
+
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+    @Autowired
+    private MediaPlayer player;
+
+    @Test
+    public void play() {
+        player.play();
+        assertEquals(
+                "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles" + System.lineSeparator(),
+                log.getLog());
+    }
+}
