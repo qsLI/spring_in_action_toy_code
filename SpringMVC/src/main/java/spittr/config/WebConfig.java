91efcb322f2936b24c4253bc2931ee70e6660404
kl <liqisheng@whu.edu.cn>
2016-05-12 16:52:17 +0800

    spring mvc  use javaconfig to set validator

--- SpringMVC/src/main/java/spittr/config/WebConfig.java
+++ SpringMVC/src/main/java/spittr/config/WebConfig.java
@@ -1,8 +1,13 @@
 package spittr.config;
 
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,7 +18,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  * Created by KL on 2016/5/6.
  */
 @Configuration
-@EnableWebMvc
+@EnableWebMvc // in spring 4 do the job for @AnnotationDrivenConfig
 @ComponentScan("spittr")
 public class WebConfig extends WebMvcConfigurerAdapter{
     @Bean
@@ -30,4 +35,32 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+
+    @Override
+    public Validator getValidator() {
+        try {
+            return validator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() throws Exception {
+        final LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+//        bean.setValidationMessageSource(messageSource());
+        bean.setProviderClass(HibernateValidator.class);
+        return bean;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/WEB-INF/messages/validation");
+        messageSource.setCacheSeconds(3600);
+        return messageSource;
+    }
+
+
 }
