e521f0e3a9346cb4737ceaff044b27b133327a6d
kl <liqisheng@whu.edu.cn>
2016-05-12 17:41:39 +0800

    spring mvc static resources

--- SpringMVC/src/main/java/spittr/config/WebConfig.java
+++ SpringMVC/src/main/java/spittr/config/WebConfig.java
@@ -9,9 +9,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
@@ -62,5 +60,9 @@ public class WebConfig extends WebMvcConfigurerAdapter{
         return messageSource;
     }
 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 
 }
